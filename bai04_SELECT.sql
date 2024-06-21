7310ae0b9e42f1c2f2bd4ca9f621328f6750df7f
Huỳnh Lê Bảo <huynhlebaoes@gmail.com>
2024-06-21 14:26:37 +0700

    Add files via upload

---
+++ bai04_SELECT.sql
@@ -0,0 +1,60 @@
+-- Bài 04: Câu lệnh truy vấn SELECT. --
+
+-- Phần I: Câu lệnh SELECT  --
+-- A. Ví dụ.
+-- Ví dụ 01: Viết câu lệnh SQL lấy ra tên của tất cả các sản phẩm.
+SELECT ProductName
+FROM Products;
+GO
+-- Ví dụ 02: Viết câu lệnh SQL lấy ra tên sản phẩm, giá bán trên mỗi đơn vị 
+-- và số lượng sản phẩm trên đơn vị.
+SELECT ProductName, UnitPrice, QuantityPerUnit
+FROM Products;
+GO
+-- Ví dụ 03: Viết câu lệnh SQL lấy ra tên công ty của khách hàng và quốc gia của khách hàng đó.
+SELECT CompanyName, Country
+FROM Customers;
+GO
+
+-- B. Bài tập.
+-- Bài tập 01: Viết câu lệnh SQL lấy ra tên công ty và số điện thoại của tất cả các nhà cung cấp hàng
+SELECT CompanyName, Phone
+FROM Suppliers;
+GO
+
+-- C. Thử thách.
+-- Thử thách 1: Viết câu lệnh SQL lấy ra mã người giao hàng và tên công ty của tất cả người giao hàng
+SELECT ShipperID, CompanyName
+FROM Shippers;
+GO
+-- Thử thách 2: Viết câu lệnh SQL lấy ra họ, tên và quốc gia của tất cả các nhân viên
+SELECT FirstName, LastName, Country
+FROM Employees;
+GO
+
+-- Phần II: Câu lệnh SELECT *  --
+-- A. Ví dụ.
+-- Ví dụ 01: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng Products
+SELECT *
+FROM [dbo].[Products];
+GO
+-- Ví dụ 02: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng Customers
+SELECT *
+FROM [dbo].[Customers];
+GO
+
+-- B. Bài tập.
+-- Bài tập 01: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng Suppliers
+SELECT *
+FROM Suppliers;
+GO
+
+-- C. Thử thách:
+-- Thử thách 01: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng Orders
+SELECT *
+FROM Orders;
+GO
+-- Thử thách 02: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng Categories
+SELECT *
+FROM Categories;
+GO
