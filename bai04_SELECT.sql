032b1505732adb4cf74cd42ee3260bd508f7161f
Huỳnh Lê Bảo <huynhlebaoes@gmail.com>
2024-06-23 06:42:10 +0700

    Update bai04_SELECT.sql

--- bai04_SELECT.sql
+++ bai04_SELECT.sql
@@ -2,58 +2,59 @@
 
 -- Phần I: Câu lệnh SELECT.  --
 -- A. Ví dụ. --
--- Ví dụ 01: Viết câu lệnh SQL lấy ra tên của tất cả các sản phẩm từ bảng Products.
+-- Ví dụ 01: Viết câu lệnh SQL lấy ra tên của tất cả các sản phẩm (ProductName) từ bảng "Products".
 SELECT ProductName
 FROM Products;
 GO
--- Ví dụ 02: Viết câu lệnh SQL lấy ra tên sản phẩm, giá bán trên mỗi đơn vị và số lượng sản phẩm trên đơn vị từ bảng Products.
+/* Ví dụ 02: Viết câu lệnh SQL lấy ra tên sản phẩm (ProductName), giá bán trên mỗi đơn vị (UnitPrice) và số lượng sản phẩm trên đơn vị (QuantityPerUnit)
+từ bảng "Products". */
 SELECT ProductName, UnitPrice, QuantityPerUnit
 FROM Products;
 GO
--- Ví dụ 03: Viết câu lệnh SQL lấy ra tên công ty của khách hàng và quốc gia của khách hàng đó từ bảng Customers.
+-- Ví dụ 03: Viết câu lệnh SQL lấy ra tên công ty của khách hàng (CompanyName) và quốc gia của khách hàng (Country) đó từ bảng "Customers".
 SELECT CompanyName, Country
 FROM Customers;
 GO
   
 -- B. Bài tập. --
--- Bài tập 01: Viết câu lệnh SQL lấy ra tên công ty và số điện thoại của tất cả các nhà cung cấp từ bảng Suppliers.
+-- Bài tập 01: Viết câu lệnh SQL lấy ra tên công ty (CompanyName) và số điện thoại (Phone) của tất cả các nhà cung cấp từ bảng "Suppliers".
 SELECT CompanyName, Phone
 FROM Suppliers;
 GO
   
 -- C. Thử thách. --
--- Thử thách 1: Viết câu lệnh SQL lấy ra mã người giao hàng và tên công ty từ bảng Shippers.
+-- Thử thách 1: Viết câu lệnh SQL lấy ra mã người giao hàng (ShipperID) và tên công ty (CompanyName) từ bảng "Shippers".
 SELECT ShipperID, CompanyName
 FROM Shippers;
 GO
--- Thử thách 2: Viết câu lệnh SQL lấy ra họ, tên và quốc gia của tất cả các nhân viên từ bảng Employees.
+-- Thử thách 2: Viết câu lệnh SQL lấy ra họ (LastName), tên (FirstName) và quốc gia (Country) của tất cả các nhân viên từ bảng "Employees".
 SELECT LastName, FirstName, Country
 FROM Employees;
 GO
 
 -- Phần II: Câu lệnh SELECT *.  --
 -- A. Ví dụ. --
--- Ví dụ 01: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng Products.
+-- Ví dụ 01: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng "Products".
 SELECT *
 FROM [dbo].[Products];
 GO
--- Ví dụ 02: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng Customers.
+-- Ví dụ 02: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng "Customers".
 SELECT *
 FROM [dbo].[Customers];
 GO
   
 -- B. Bài tập.--
--- Bài tập 01: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng Suppliers.
+-- Bài tập 01: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng "Suppliers".
 SELECT *
 FROM Suppliers;
 GO
   
 -- C. Thử thách. --
--- Thử thách 01: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng Orders.
+-- Thử thách 01: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng "Orders".
 SELECT *
 FROM Orders;
 GO
--- Thử thách 02: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng Categories.
+-- Thử thách 02: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ bảng "Categories".
 SELECT *
 FROM Categories;
 GO
