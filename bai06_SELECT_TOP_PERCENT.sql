497af832276e3c533ca91925a480fa613df2df33
Huỳnh Lê Bảo <huynhlebaoes@gmail.com>
2024-06-22 21:14:04 +0700

    Update bai06_SELECT_TOP_PERCENT.sql

--- bai06_SELECT_TOP_PERCENT.sql
+++ bai06_SELECT_TOP_PERCENT.sql
@@ -9,15 +9,13 @@ GO
 SELECT TOP 30 PERCENT *
 FROM Employees;
 GO
-/* Ví dụ 03: Viết câu lệnh SQL lấy ra các mã khách hàng (CustomerID) không bị trùng lặp 
-và chỉ lấy ra 5 dòng đầu tiên trong bảng Orders. */
+-- Ví dụ 03: Viết câu lệnh SQL lấy ra các mã khách hàng (CustomerID) không bị trùng lặp và chỉ lấy ra 5 dòng đầu tiên trong bảng Orders. 
 SELECT DISTINCT TOP 5 CustomerID
 FROM Orders;
 GO
 
 -- B. Bài tập. --
-/* Bài tập 01: Viết câu lệnh SQL lấy ra các sản phẩm có mã thể loại (CategoryID) không bị trùng lặp 
-và chỉ lấy ra 3 dòng đầu tiên trong bảng Products. */
+-- Bài tập 01: Viết câu lệnh SQL lấy ra các sản phẩm có mã thể loại (CategoryID) không bị trùng lặp và chỉ lấy ra 3 dòng đầu tiên trong bảng Products. 
 SELECT DISTINCT TOP 3 CategoryID
 FROM Products;
 GO
@@ -27,8 +25,7 @@ GO
 SELECT TOP 2 *
 FROM Shippers;
 GO
-/* Thử thách 2: Viết câu lệnh SQL lấy ra các quốc gia (Country) khác nhau từ các nhà cunng cấp
-và chỉ lấy ra 20% số dòng trong bảng Suppliers. */
+-- Thử thách 2: Viết câu lệnh SQL lấy ra các quốc gia (Country) khác nhau từ các nhà cunng cấpvà chỉ lấy ra 20% số dòng trong bảng Suppliers. --
 SELECT DISTINCT TOP 20 PERCENT Country
 FROM Suppliers;
 GO
