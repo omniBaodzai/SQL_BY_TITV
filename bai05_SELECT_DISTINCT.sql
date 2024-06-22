779d7b69ca1c6ef71f69ad4204633d8a4e5a94da
Huỳnh Lê Bảo <huynhlebaoes@gmail.com>
2024-06-22 21:07:13 +0700

    Update bai05_SELECT_DISTINCT.sql

--- bai05_SELECT_DISTINCT.sql
+++ bai05_SELECT_DISTINCT.sql
@@ -9,23 +9,22 @@ GO
 SELECT DISTINCT PostalCode  
 FROM Suppliers;
 GO
-/* Ví dụ 03: Viết câu lệnh SQl lấy ra các dữ liệu khác nhau về họ (LastName) và chức danh lịch sự (TitleOfCourtesy)
-của nhân viên từ bảng Employees. */
+-- Ví dụ 03: Viết câu lệnh SQl lấy ra các dữ liệu khác nhau về họ (LastName) và chức danh lịch sự (TitleOfCourtesy) của các nhân viên từ bảng Employees.
 SELECT DISTINCT LastName, TitleOfCourtesy 
 FROM Employees;
 GO
+  
 -- B. Bài tập. --
 -- Bài tập 01: Viết câu lệnh SQL lấy ra các mã đơn vị vận chuyển (ShipVia) khác nhau của các đơn hàng từ bảng Orders.
 SELECT DISTINCT ShipVia
 FROM Orders;
 GO
 -- C. Thử thách. --
-/* Thử thách 1: Viết câu lệnh SQL lấy ra các mã danh mục/thể loại (CategoryID) khác nhau của các sản phẩm 
-từ bảng Products. */
+-- Thử thách 1: Viết câu lệnh SQL lấy ra các mã danh mục/thể loại (CategoryID) khác nhau của các sản phẩm từ bảng Products. 
 SELECT DISTINCT CategoryID
 FROM Products;
 GO
--- Thử thách 2: Viết câu lệnh SQL lấy ra các số lượng (Quantity) khác nhau từ bảng Order Details.
+-- Thử thách 2: Viết câu lệnh SQL lấy ra số lượng (Quantity) khác nhau từ bảng Order Details.
 SELECT DISTINCT Quantity
-FROM [Order Details]; -- Do tên bảng có khoảng trắng nên chúng ta dùng ngoặc vuông [] để đảm bảo cú pháp đúng
+FROM [Order Details]; -- Do tên bảng có khoảng trắng nên chúng ta dùng ngoặc vuông [] để đảm bảo cú pháp đúng.
 GO
