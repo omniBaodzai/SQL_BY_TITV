3924e60d8263d1434e6e3391d84e0e89751bd6af
Huỳnh Lê Bảo <huynhlebaoes@gmail.com>
2024-06-26 22:55:57 +0700

    Update bai09_COUNT_SUM_AVG.sql

--- bai09_COUNT_SUM_AVG.sql
+++ bai09_COUNT_SUM_AVG.sql
@@ -13,16 +13,15 @@ GO
 SELECT SUM(Freight) AS [TotalFreight]
 FROM Orders;
 GO
-/* Ví dụ 03: Viết câu lệnh SQL tính trung bình số lượng đơn đặt hàng (Quantity) của các sản phẩm
-từ bảng "Orders Details". */
+-- Ví dụ 03: Viết câu lệnh SQL tính trung bình số lượng đơn đặt hàng (Quantity) của các sản phẩm từ bảng "Orders Details". 
 SELECT AVG(Quantity) AS [AvgQuantity]
 FROM [Order Details];
 GO
 /* Ví dụ 04: Viết câu lệnh SQL đếm số lượng sản phầm (ProductID), tính tổng số lượng hàng trong kho (UnitsInStock)
 và tính trung bình đơn giá (UnitPrice) của các sản phẩm từ bảng "Product". */
 SELECT COUNT(*) AS [NumbersOfProducts],
-	   SUM(UnitsInStock) AS [TotalUnitsInStock],
-	   AVG(UnitPrice) AS [AvgUnitPrice]
+       SUM(UnitsInStock) AS [TotalUnitsInStock],
+       AVG(UnitPrice) AS [AvgUnitPrice]
 FROM Products;
 GO
 
@@ -39,7 +38,7 @@ GO
 /* Bài tập 02: Viết câu lệnh SQL tính trung bình cho cột "UnitPrice" và tính tổng cho cột "Quantity"
 từ bảng "Order Details". */
 SELECT AVG(UnitPrice) AS [AvgUnitPrice],
-	   SUM(Quantity) AS [TotalQuantity]
+       SUM(Quantity) AS [TotalQuantity]
 FROM [Order Details];
 GO
 
@@ -49,17 +48,15 @@ GO
 SELECT COUNT(*) AS [NumbersOfShippers]
 FROM Shippers;
 GO
--- Cách 2: Dùng mã người giao hàng (ShipperID)
+-- Cách 2: Dùng mã người giao hàng (ShipperID).
 SELECT COUNT(ShipperID) AS [NumbersOfShippers]
 FROM Shippers;
 GO
-/* Thử thách 02: Viết câu lệnh SQL tính tổng số đơn đang đặt hàng (UnitsOnOrder) của các sản phẩm
-từ bảng "Products". */
+-- Thử thách 02: Viết câu lệnh SQL tính tổng số đơn đang đặt hàng (UnitsOnOrder) của các sản phẩm từ bảng "Products". 
 SELECT SUM(UnitsOnOrder) AS [TotalUnitsOnOrder]
 FROM Products;
 GO
-/* Thử thách 03: Viết câu lệnh SQL tính trung bình số đơn được đặt hàng lại (ReorderLevel) của các sản phẩm
-từ bảng "Products". */
+-- Thử thách 03: Viết câu lệnh SQL tính trung bình số đơn được đặt hàng lại (ReorderLevel) của các sản phẩm từ bảng "Products". 
 SELECT AVG(ReorderLevel) AS [AvgReorderLevel]
 FROM Products;
 GO
