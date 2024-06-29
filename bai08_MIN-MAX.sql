c1adff80986b898051c24a5123f8d25f863f8206
Huỳnh Lê Bảo <huynhlebaoes@gmail.com>
2024-06-29 09:01:39 +0700

    Update bai08_MIN-MAX.sql

--- bai08_MIN-MAX.sql
+++ bai08_MIN-MAX.sql
@@ -1,7 +1,7 @@
 -- Bài 08: Giá trị MIN MAX. --
 
 -- A. Ví dụ. --
--- Ví dụ 01: Viết câu lệnh SQL lấy ra giá (UnitPrice) thấp nhất của các sản phẩm từ bảng "Products".
+-- Ví dụ 01: Viết câu lệnh SQL lấy ra giá (UnitPrice) thấp nhất của các sản phẩm từ [bảng Products].
 SELECT MIN(UnitPrice) AS [MỉnPrice]
 FROM Products;
 GO
@@ -9,24 +9,24 @@ GO
 SELECT MIN(UnitPrice) -- Không có tên cột (No column name).
 FROM Products;
 GO
--- Ví dụ 02: Viết câu lệnh SQL lấy ra ngày đặt hàng (OrderDate) gần nhất từ bảng "Orders".
+-- Ví dụ 02: Viết câu lệnh SQL lấy ra ngày đặt hàng (OrderDate) gần nhất từ [bảng Orders].
 SELECT MAX(OrderDate) AS [MaxOrderDate]
 FROM Orders;
 GO
--- Ví dụ 03: Viết câu lệnh SQL lấy ra số lượng sản phẩm tồn kho (UnitsInStock) lớn nhất từ bảng "Products".
+-- Ví dụ 03: Viết câu lệnh SQL lấy ra số lượng sản phẩm trong kho (UnitsInStock) lớn nhất từ [bảng Products].
 SELECT MAX(UnitsInStock) AS MaxUnitsInStock
 FROM Products;
 GO
 
 -- B. Bài tập. --
--- Bài tập 01: Viết câu lệnh SQL lấy ra nhân viên có độ tuổi lớn nhất trong công ty từ bảng "Employees".
+-- Bài tập 01: Viết câu lệnh SQL lấy ra nhân viên có độ tuổi lớn nhất trong công ty từ [bảng Employees].
 SELECT MIN(BirthDate) AS MinBirthDate 
 FROM Employees;
 GO
 
 -- C. Thử thách. --
 /* Thử thách 01: Viết câu lệnh SQL lấy ra số lượng đơn hàng (Quantity) khác nhau và chọn ra đơn hàng 
-có số lượng ít nhất từ bảng "Order Details". */
+có số lượng ít nhất từ [bảng Order Details]. */
 SELECT DISTINCT MIN(Quantity) AS MinQuantity
 FROM [Order Details];
 GO
@@ -35,7 +35,7 @@ SELECT DISTINCT TOP 20 PERCENT MIN(Quantity)
 FROM [Order Details];
 GO
 /* Thử thách 02: Viết câu lệnh SQL lấy ra nhân viên có ngày bắt đầu đi làm tại công ty muộn nhất (HireDate)
-từ bảng "Employees". */
+từ [bảng Employees]. */
 SELECT MAX(HireDate) AS [HireDate]
 FROM Employees; 
 GO
